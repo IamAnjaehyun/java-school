94b92956e9fabf8a554b18ee89b4c76a66d6a330
IamAnjaehyun <lvet0330@gmail.com>
2022-10-23 00:35:06 +0900

    ~week6

---
+++ java-school-repeat/src/week5/week5_1.java
@@ -0,0 +1,46 @@
+package week5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class week5_1 {
+    public static void main(String[] args) {
+        String str = "";
+        int sco[][] = new int[20][6];
+        int r = 0, c = 0, tot = 0, rank = 0;
+
+        try {
+            File fin = new File("score_in.txt");
+            Scanner sc = new Scanner(fin);
+
+            while (sc.hasNextLine()) {
+                str = sc.nextLine();
+                String st[] = str.split(" ");
+                r++;
+                tot = 0;
+                for (c = 0; c < st.length; c++) {
+                    sco[r][c] = Integer.parseInt(st[c]);
+                    tot = sco[r][1]+sco[r][2]+sco[r][3];
+                }
+                sco[r][4] = tot;
+            }
+            for (int i = 1; i <= r; i++) {
+                rank = 1;
+                for (int j = 1; j <=r; j++) {
+                    if(sco[i][4]<sco[j][4]) rank++;
+                }
+                sco[i][5] = rank;
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("파일을 찾을 수 없습니다.");
+        }
+        for (int i = 0; i <= r; i++) {
+            for (int j = 0; j < sco[0].length; j++) {
+                System.out.printf("%4d", sco[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ java-school-repeat/src/week6/week6_1.java
@@ -0,0 +1,26 @@
+package week6;
+
+import java.util.Arrays;
+//Lotto
+public class week6_1 {
+    public static void main(String[] args) {
+        int i;
+        int j[] = new int[7];
+        int k;
+        j[0] = (int)(Math.random()*45+1);
+        for(i=1; i<=6; i++) {
+            j[i] = (int)(Math.random()*45+1);
+
+            for(k=1; k<i; k++) {
+                if(j[i] == j[k]) {
+                    i--;
+                    continue;
+                }
+            }
+        }
+        Arrays.sort(j);
+        for(i=0;i<=6;i++) {
+            System.out.println(j[i]+" ");
+        }
+    }
+}
